c7ca45f997a669b19b316c4f66b454982ce7f707
Massimo Puddu <mssmpuddu@gmail.com>
2022-02-03 10:44:20 +0100

    Creazione dell'app toDo

--- App.js
+++ App.js
@@ -1,13 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {TextInput, Button} from "react-native-web";
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  state = {
+    text: '',
+  }
+  render() {
+    return (
+        <View style={styles.container}>
+          <View style={styles.InputContainer}>
+            {/* Input */}
+            <TextInput placeholder={"scrivi todo"} style={styles.Input}/>
+            <Button
+                title={"Invio"}
+                onPress={() => {}}
+            />
+          </View>
+          <View>
+            {/* Output */}
+            <Text>
+
+            </Text>
+          </View>
+        </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +34,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
+    Input: {
+      width: '80%',
+      borderWidth: 1,
+      borderRadius: 5,
+      paddingVertical: 5,
+      paddingLeft: 3
+    },
+  InputContainer: {
+    marginTop: 50,
+    padding: 50,
+    flexDirection: "row",
+    alignItems: "center"
+  }
 });
